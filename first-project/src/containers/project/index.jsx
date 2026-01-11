786459d4cc29c444401f39221e8cd5cf21a006c3
Mahek <maheksondagar8@gmail.com>
2026-01-11 14:51:41 +0530

    project json create

--- first-project/src/containers/project/index.jsx
+++ first-project/src/containers/project/index.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import { BsInfoCircleFill } from 'react-icons/bs'
 import PageHeaderContain from "../../components/pageHeaderContent";
 import './styles.scss'
-
-const projectSummary = "Designed and developed a personal portfolio website using React to showcase projects, skills, and experience. Implemented reusable components, responsive layouts, and clean UI with smooth navigation and performance-focused rendering."
-const projectKeyPoint = "HTML,SCSS,React Js"
+import { projects } from './utils'
 
 const Projects = () => {
     return (
@@ -14,21 +12,32 @@ const Projects = () => {
                 icon={<BsInfoCircleFill size={40} />} />
 
             <div className="project_content">
-                <div className="project_content_infoWrapper">
-                    <h2>My Portfolio</h2>
-                    <div className="project_content_infoWrapper_keypoints">
-                        <span>{projectKeyPoint}</span>
-                    </div>
-                    <p>{projectSummary}</p>
-                    <div className="project_content_infoWrapper_codelink">
-                        <a
-                            href="https://github.com/maheksondagar8-prog/My-protfolio"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            View Source Code
-                        </a>
-                    </div>
+                <div className="project_content">
+                    {
+                        projects.map((project, i) => (
+                            <div className="project_content_infoWrapper" key={i}>
+                                <h2>{project.title}</h2>
+
+                                <div className="project_content_infoWrapper_keypoints">
+                                    {project.keyPoints.map((skill, i) => (
+                                        <span key={i}>{skill}</span>
+                                    ))}
+                                </div>
+
+                                <p>{project.summary}</p>
+
+                                <div className="project_content_infoWrapper_codelink">
+                                    <a
+                                        href={project.codeLink}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        View Source Code
+                                    </a>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </section>
---
+++ first-project/src/containers/project/utils.js
@@ -0,0 +1,16 @@
+export const projects = [
+    {
+        title: "My Portfolio",
+        summary:
+            "Designed and developed a personal portfolio website using React to showcase projects, skills, and experience. Implemented reusable components, responsive layouts, and clean UI with smooth navigation and performance-focused rendering.",
+        keyPoints: ["HTML", "SCSS", "React JS"],
+        codeLink: "https://github.com/maheksondagar8-prog/My-protfolio"
+    },
+    {
+        title: "Time Table Genrator",
+        summary:
+            "Designed and developed a web-based Student and Faculty Timetable Management System using PHP and MySQL (XAMPP) as part of a college final-year project. The system allows administrators to create and manage timetable tables, assign subjects, faculty, and classrooms efficiently. It helps students and faculty view structured schedules, reduces manual errors, and improves timetable organization through a centralized database-driven approach.",
+        keyPoints: ["PHP", "MySQL", "XAMPP", "HTML", "CSS"],
+        codeLink: "https://github.com/maheksondagar8-prog/time-table-genrator"
+    }
+]
