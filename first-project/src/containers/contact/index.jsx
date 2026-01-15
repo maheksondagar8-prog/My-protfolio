4d6b129465ba742eb0ce762e27ce0ac8f606d8e8
Mahek <maheksondagar8@gmail.com>
2026-01-15 22:43:36 +0530

    contact us page implement

--- first-project/src/containers/contact/index.jsx
+++ first-project/src/containers/contact/index.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import { BsInfoCircleFill } from 'react-icons/bs'
 import PageHeaderContain from "../../components/pageHeaderContent";
+import './styles.scss';
 
 const ContactUs = () => {
     return (
-        <section className="about">
+        <section className="contact">
             <PageHeaderContain
                 headerText="My Contact"
                 icon={<BsInfoCircleFill size={40} />} />
+
+            <div className="contact_content">
+                <h3 className="contact_content_header-text">Let`s Talk</h3>
+                <div className="contact_content_form_controlwrapper">
+                    <div className="nameWrapper">
+                        <input required name="name" className="inputName" type={'text'}></input>
+                        <label htmlFor="name" className="nameLabel">Name</label>
+                    </div>
+                    <div className="emailWrapper">
+                        <input required name="email" className="inputEmail" type={'text'}></input>
+                        <label htmlFor="email" className="emailLabel">Email</label>
+                    </div>
+                    <div className="descriptionWrapper">
+                        <textarea required name="description" className="inputDescription" type={'text'} rows={5}></textarea>
+                        <label htmlFor="description" className="descriptionLabel">Description</label>
+                    </div>
+                </div>
+                <button>Submit</button>
+            </div>
         </section>
     )
 }
--- first-project/src/containers/home/index.jsx
+++ first-project/src/containers/home/index.jsx
@@ -1,6 +1,6 @@
 import './styles.scss';
 import { useNavigate } from 'react-router-dom'
-
+import { FaLinkedin, FaGithub, FaFileDownload } from "react-icons/fa";
 const Home = () => {
 
     const navigate = useNavigate()
@@ -18,7 +18,43 @@ const Home = () => {
             </div>
 
             <div className="home_contact-me">
+                {/* Hire Me button */}
                 <button onClick={handleNavigateToContactMePage}>Hire Me</button>
+
+                {/* LinkedIn */}
+                <div className="home_contact-me_social-media">
+                    <a
+                        href="https://www.linkedin.com/in/mahek-sondagar-470406247"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="home_contact-me_social-media_icon-btn-linkedin"
+                    >
+                        <FaLinkedin size={22} />
+                    </a>
+                </div>
+
+                {/* GitHub */}
+                <div className="home_contact-me_social-media">
+                    <a
+                        href="https://github.com/maheksondagar8-prog"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="home_contact-me_social-media_icon-btn-github"
+                    >
+                        <FaGithub size={22} />
+                    </a>
+                </div>
+
+                {/* Download CV */}
+                <div className="home_contact-me_social-media">
+                    <a
+                        href="/Mahek_sondagar_resume.pdf"
+                        download="Mahek_sondagar_resume.pdf"
+                        className="home_contact-me_social-media_icon-btn-cv"
+                    >
+                        <FaFileDownload size={22} />
+                    </a>
+                </div>
             </div>
 
         </section>
