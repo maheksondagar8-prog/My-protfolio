e0e9222ae767e06a813c739e9600a9df25cce14c
Mahek <maheksondagar8@gmail.com>
2026-01-16 21:30:15 +0530

    contact us Mail service implement

--- first-project/src/containers/contact/index.jsx
+++ first-project/src/containers/contact/index.jsx
@@ -1,16 +1,44 @@
 import React from "react";
 import { BsInfoCircleFill } from 'react-icons/bs'
 import PageHeaderContain from "../../components/pageHeaderContent";
+import emailjs from "emailjs-com";
+import { useRef } from "react";
 import './styles.scss';
 
 const ContactUs = () => {
+    const formRef = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                "service_etxv1xm",
+                "template_p7m0lek",
+                formRef.current,
+                "4JYi2Qu2MwINrYNBH"
+            )
+            .then(
+                (result) => {
+                    alert("Message sent successfully!");
+                    formRef.current.reset();
+                },
+                (error) => {
+                    alert("Something went wrong!");
+                }
+            );
+    };
     return (
         <section className="contact">
             <PageHeaderContain
                 headerText="My Contact"
                 icon={<BsInfoCircleFill size={40} />} />
 
-            <div className="contact_content">
+            <form
+                ref={formRef}
+                onSubmit={sendEmail}
+                className="contact_content">
+
                 <h3 className="contact_content_header-text">Let`s Talk</h3>
                 <div className="contact_content_form_controlwrapper">
                     <div className="nameWrapper">
@@ -18,7 +46,7 @@ const ContactUs = () => {
                         <label htmlFor="name" className="nameLabel">Name</label>
                     </div>
                     <div className="emailWrapper">
-                        <input required name="email" className="inputEmail" type={'text'}></input>
+                        <input required name="email" className="inputEmail" type={'email'}></input>
                         <label htmlFor="email" className="emailLabel">Email</label>
                     </div>
                     <div className="descriptionWrapper">
@@ -27,7 +55,7 @@ const ContactUs = () => {
                     </div>
                 </div>
                 <button>Submit</button>
-            </div>
+            </form>
         </section>
     )
 }
